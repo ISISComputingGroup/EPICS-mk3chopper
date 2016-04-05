653bb85b402ce6afbee0873ae2fbadea1802b792
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2016-04-05 17:04:27 +0100

    Fix error status checking

--- build.bat
+++ build.bat
@@ -5,32 +5,29 @@ mkdir bin\%EPICS_HOST_ARCH%
 mkdir lib\%EPICS_HOST_ARCH%
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-debug" (
+    set BUILD_DIR=x64\Debug
     call msbuild.exe /p:Configuration=Debug;Platform=x64 Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
-    copy x64\Debug\*.lib lib\%EPICS_HOST_ARCH%
-    copy x64\Debug\*.dll bin\%EPICS_HOST_ARCH%
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64" (
+    set BUILD_DIR=x64\Release
     call msbuild.exe /p:Configuration=Release;Platform=x64 Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
-    copy x64\Release\*.lib lib\%EPICS_HOST_ARCH%
-    copy x64\Release\*.dll bin\%EPICS_HOST_ARCH%
 )
     
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-debug" (
+    set BUILD_DIR=Debug
     call msbuild.exe /p:Configuration=Debug;Platform=x86 Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
-    copy Debug\*.lib lib\%EPICS_HOST_ARCH%
-    copy Debug\*.dll bin\%EPICS_HOST_ARCH%
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86" (
+    set BUILD_DIR=Release
     call msbuild.exe /p:Configuration=Release;Platform=x86 Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
-    copy Release\*.lib lib\%EPICS_HOST_ARCH%
-    copy Release\*.dll bin\%EPICS_HOST_ARCH%
 )
+
+if %ERRORLEVEL% neq 0 goto PROBLEM
+
+copy %BUILD_DIR%\*.lib lib\%EPICS_HOST_ARCH%
+copy %BUILD_DIR%\*.dll bin\%EPICS_HOST_ARCH%
 
 GOTO :EOF
 
--- unbuild.bat
+++ unbuild.bat
@@ -2,23 +2,21 @@
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64-debug" (
     call msbuild.exe /p:Configuration=Debug;Platform=x64 /t:clean Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
 )
 
 if /I "%EPICS_HOST_ARCH%" == "windows-x64" (
     call msbuild.exe /p:Configuration=Release;Platform=x64 /t:clean Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
 )
     
 if /I "%EPICS_HOST_ARCH%" == "win32-x86-debug" (
     call msbuild.exe /p:Configuration=Debug;Platform=x86 /t:clean Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
 )
 
 if /I "%EPICS_HOST_ARCH%" == "win32-x86" (
     call msbuild.exe /p:Configuration=Release;Platform=x86 /t:clean Mk3_Chopper.sln
-	if %ERRORLEVEL% neq 0 goto PROBLEM
 )
+
+if %ERRORLEVEL% neq 0 goto PROBLEM
 
 GOTO :EOF
 
