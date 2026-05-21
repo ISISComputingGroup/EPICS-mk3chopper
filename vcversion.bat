39527d4d26c73f4b54d172beb8db0df7c9a2dc39
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2026-05-21 22:27:37 +0100

    Update for VS2026

--- vcversion.bat
+++ vcversion.bat
@@ -8,12 +8,18 @@ for /F "tokens=1,2,3 delims=. " %%a in ("%VSCMD_VER%") do (
    set VS_SU=%%c
 )
 
+if "%VS_MAJOR%" == "18" (
+    REM VS2026
+    set PLATFORM_TOOLSET=v144
+    set TARGET_FRAMEWORK_VERSION=v4.7.2
+    exit /b 0
+)
 if "%VS_MAJOR%" == "17" (
     REM VS2022
     set PLATFORM_TOOLSET=v143
     set TARGET_FRAMEWORK_VERSION=v4.7.2
-) else (
-    REM VS2019
-    set PLATFORM_TOOLSET=v142
-    set TARGET_FRAMEWORK_VERSION=v4.0
+    exit /b 0
 )
+REM VS2019
+set PLATFORM_TOOLSET=v142
+set TARGET_FRAMEWORK_VERSION=v4.0
